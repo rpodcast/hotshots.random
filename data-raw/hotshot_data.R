1a8b3eb2bfa1ba088c0d0a9a2e01496d956091db
Eric Nantz <theRcast@gmail.com>
2021-02-05 17:00:55 +0000

    tweak url column names

--- data-raw/hotshot_data.R
+++ data-raw/hotshot_data.R
@@ -4,12 +4,12 @@ hotshot_data <- list(
     drivers = list(
         alexa = list(
             driver_name = "alexa",
-            img_url = "https://i.imgur.com/nGh4oUH.png",
+            driver_img_url = "https://i.imgur.com/nGh4oUH.png",
             country = "us",
             cars = list(
                 list(
                     car_name = "stallion",
-                    img_url = "https://i.imgur.com/fPPYtQ0.png",
+                    car_img_url = "https://i.imgur.com/fPPYtQ0.png",
                     type = "balanced",
                     speed = 7,
                     acceleration = 7,
@@ -17,7 +17,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "thunder",
-                    img_url = "https://i.imgur.com/vETmvkN.png",
+                    car_img_url = "https://i.imgur.com/vETmvkN.png",
                     type = "acceleration",
                     speed = 5,
                     acceleration = 9,
@@ -25,7 +25,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "mirage",
-                    img_url = "https://i.imgur.com/3Etcc4C.png",
+                    car_img_url = "https://i.imgur.com/3Etcc4C.png",
                     type = "speed",
                     speed = 9,
                     acceleration = 6,
@@ -33,7 +33,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "diamond_back",
-                    img_url = "https://i.imgur.com/0TUg2zZ.png",
+                    car_img_url = "https://i.imgur.com/0TUg2zZ.png",
                     type = "drift",
                     speed = 5,
                     acceleration = 8,
@@ -43,12 +43,12 @@ hotshot_data <- list(
         ),
         aston = list(
             driver_name = "aston",
-            img_url = "https://i.imgur.com/5KyknYM.png",
+            driver_img_url = "https://i.imgur.com/5KyknYM.png",
             country = "gb",
             cars = list(
                 list(
                     car_name = "bulldog",
-                    img_url = "https://i.imgur.com/liS0suC.png",
+                    car_img_url = "https://i.imgur.com/liS0suC.png",
                     type = "balanced",
                     speed = 8,
                     acceleration = 8,
@@ -56,7 +56,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "bandit",
-                    img_url = "https://i.imgur.com/fFGgAu5.png",
+                    car_img_url = "https://i.imgur.com/fFGgAu5.png",
                     type = "acceleration",
                     speed = 7,
                     acceleration = 9,
@@ -64,7 +64,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "avenger",
-                    img_url = "https://i.imgur.com/zE5q70i.png",
+                    car_img_url = "https://i.imgur.com/zE5q70i.png",
                     type = "speed",
                     speed = 10,
                     acceleration = 5,
@@ -72,7 +72,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "shadow",
-                    img_url = "https://i.imgur.com/9ZsVjhN.png",
+                    car_img_url = "https://i.imgur.com/9ZsVjhN.png",
                     type = "drift",
                     speed = 6,
                     acceleration = 7,
@@ -82,12 +82,12 @@ hotshot_data <- list(
         ),
         xing = list(
             driver_name = "xing",
-            img_url = "https://i.imgur.com/48868f2.png",
+            driver_img_url = "https://i.imgur.com/48868f2.png",
             country = "cn",
             cars = list(
                 list(
                     car_name = "fastback",
-                    img_url = "https://i.imgur.com/BAtEJJA.png",
+                    car_img_url = "https://i.imgur.com/BAtEJJA.png",
                     type = "balanced",
                     speed = 7,
                     acceleration = 8,
@@ -95,7 +95,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "blaze",
-                    img_url = "https://i.imgur.com/RmJg3U6.png",
+                    car_img_url = "https://i.imgur.com/RmJg3U6.png",
                     type = "acceleration",
                     speed = 6,
                     acceleration = 9,
@@ -103,7 +103,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "alpha",
-                    img_url = "https://i.imgur.com/C1qitYu.png",
+                    car_img_url = "https://i.imgur.com/C1qitYu.png",
                     type = "speed",
                     speed = 10,
                     acceleration = 7,
@@ -111,7 +111,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "sentinel",
-                    img_url = "https://i.imgur.com/kMNcFEw.png",
+                    car_img_url = "https://i.imgur.com/kMNcFEw.png",
                     type = "drift",
                     speed = 6,
                     acceleration = 6,
@@ -121,12 +121,12 @@ hotshot_data <- list(
         ),
         keiko = list(
             driver_name = "keiko",
-            img_url = "https://i.imgur.com/2EzeVsi.png",
+            driver_img_url = "https://i.imgur.com/2EzeVsi.png",
             country = "jp",
             cars = list(
                 list(
                     car_name = "light_speed",
-                    img_url = "https://i.imgur.com/Rahw1jX.png",
+                    car_img_url = "https://i.imgur.com/Rahw1jX.png",
                     type = "balanced",
                     speed = 8,
                     acceleration = 8,
@@ -134,7 +134,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "star_fire",
-                    img_url = "https://i.imgur.com/VEjRdEQ.png",
+                    car_img_url = "https://i.imgur.com/VEjRdEQ.png",
                     type = "acceleration",
                     speed = 6,
                     acceleration = 10,
@@ -142,7 +142,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "super_f-90",
-                    img_url = "https://i.imgur.com/QDD5MiD.png",
+                    car_img_url = "https://i.imgur.com/QDD5MiD.png",
                     type = "speed",
                     speed = 10,
                     acceleration = 8,
@@ -150,7 +150,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "eight_rock",
-                    img_url = "https://i.imgur.com/OmA1CMV.png",
+                    car_img_url = "https://i.imgur.com/OmA1CMV.png",
                     type = "drift",
                     speed = 6,
                     acceleration = 6,
@@ -160,12 +160,12 @@ hotshot_data <- list(
         ),
         marcus = list(
             driver_name = "marcus",
-            img_url = "https://i.imgur.com/KvJMvWS.png",
+            driver_img_url = "https://i.imgur.com/KvJMvWS.png",
             country = "jm",
             cars = list(
                 list(
                     car_name = "vector",
-                    img_url = "https://i.imgur.com/EhaaKPL.png",
+                    car_img_url = "https://i.imgur.com/EhaaKPL.png",
                     type = "balanced",
                     speed = 6,
                     acceleration = 8,
@@ -173,7 +173,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "rosso",
-                    img_url = "https://i.imgur.com/uVBsP7V.png",
+                    car_img_url = "https://i.imgur.com/uVBsP7V.png",
                     type = "acceleration",
                     speed = 7,
                     acceleration = 9,
@@ -181,7 +181,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "carbon",
-                    img_url = "https://i.imgur.com/SUneNr9.png",
+                    car_img_url = "https://i.imgur.com/SUneNr9.png",
                     type = "speed",
                     speed = 10,
                     acceleration = 8,
@@ -189,7 +189,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "mongoose",
-                    img_url = "https://i.imgur.com/wHMSiwC.png",
+                    car_img_url = "https://i.imgur.com/wHMSiwC.png",
                     type = "drift",
                     speed = 8,
                     acceleration = 5,
@@ -199,12 +199,12 @@ hotshot_data <- list(
         ),
         viktor = list(
             driver_name = "viktor",
-            img_url = "https://i.imgur.com/XKK7GMi.png",
+            driver_img_url = "https://i.imgur.com/XKK7GMi.png",
             country = "ru",
             cars = list(
                 list(
                     car_name = "blade",
-                    img_url = "https://i.imgur.com/DROEzT5.png",
+                    car_img_url = "https://i.imgur.com/DROEzT5.png",
                     type = "balanced",
                     speed = 7,
                     acceleration = 7,
@@ -212,7 +212,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "rennen",
-                    img_url = "https://i.imgur.com/q0ohlXW.png",
+                    car_img_url = "https://i.imgur.com/q0ohlXW.png",
                     type = "acceleration",
                     speed = 5,
                     acceleration = 9,
@@ -220,7 +220,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "t-66_super",
-                    img_url = "https://i.imgur.com/NY8CQxf.png",
+                    car_img_url = "https://i.imgur.com/NY8CQxf.png",
                     type = "speed",
                     speed = 9,
                     acceleration = 6,
@@ -228,7 +228,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "el_toro",
-                    img_url = "https://i.imgur.com/YrW81Gp.png",
+                    car_img_url = "https://i.imgur.com/YrW81Gp.png",
                     type = "drift",
                     speed = 7,
                     acceleration = 6,
@@ -238,12 +238,12 @@ hotshot_data <- list(
         ),
         mike = list(
             driver_name = "mike",
-            img_url = "https://i.imgur.com/HPc2dSU.png",
+            driver_img_url = "https://i.imgur.com/HPc2dSU.png",
             country = "us",
             cars = list(
                 list(
                     car_name = "athena",
-                    img_url = "https://i.imgur.com/bMMjEHB.png",
+                    car_img_url = "https://i.imgur.com/bMMjEHB.png",
                     type = "balanced",
                     speed = 8,
                     acceleration = 6,
@@ -251,7 +251,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "eagle",
-                    img_url = "https://i.imgur.com/JyMnd3a.png",
+                    car_img_url = "https://i.imgur.com/JyMnd3a.png",
                     type = "acceleration",
                     speed = 7,
                     acceleration = 10,
@@ -259,7 +259,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "patriot",
-                    img_url = "https://i.imgur.com/cI9vf04.png",
+                    car_img_url = "https://i.imgur.com/cI9vf04.png",
                     type = "speed",
                     speed = 9,
                     acceleration = 5,
@@ -267,7 +267,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "bullet",
-                    img_url = "https://i.imgur.com/UhS9vFa.png",
+                    car_img_url = "https://i.imgur.com/UhS9vFa.png",
                     type = "drift",
                     speed = 7,
                     acceleration = 5,
@@ -277,12 +277,12 @@ hotshot_data <- list(
         ),
         toshiro = list(
             driver_name = "toshiro",
-            img_url = "https://i.imgur.com/ZkZy1gu.png",
+            driver_img_url = "https://i.imgur.com/ZkZy1gu.png",
             country = "jp",
             cars = list(
                 list(
                     car_name = "furious",
-                    img_url = "https://i.imgur.com/QDkBoJn.png",
+                    car_img_url = "https://i.imgur.com/QDkBoJn.png",
                     type = "balanced",
                     speed = 8,
                     acceleration = 6,
@@ -290,7 +290,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "wild_line",
-                    img_url = "https://i.imgur.com/vsAGMEW.png",
+                    car_img_url = "https://i.imgur.com/vsAGMEW.png",
                     type = "acceleration",
                     speed = 6,
                     acceleration = 10,
@@ -298,7 +298,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "r-400_sport",
-                    img_url = "https://i.imgur.com/gIFlqeQ.png",
+                    car_img_url = "https://i.imgur.com/gIFlqeQ.png",
                     type = "speed",
                     speed = 9,
                     acceleration = 7,
@@ -306,7 +306,7 @@ hotshot_data <- list(
                 ),
                 list(
                     car_name = "sky_road",
-                    img_url = "https://i.imgur.com/hW5W1XZ.png",
+                    car_img_url = "https://i.imgur.com/hW5W1XZ.png",
                     type = "drift",
                     speed = 6,
                     acceleration = 6,
